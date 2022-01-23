4237932c8f55ed8528bb4f6b2d1dc75cd13be96b
unknown <emad.m.alnagar@gmail.com>
2022-01-23 23:31:25 +0200

    style last touch

--- project/static/js/main.js
+++ project/static/js/main.js
@@ -17,42 +17,6 @@ function closeMenu() {
     navMenu.classList.remove("active");
 }
 
-// SETTINGS BOX
-const toggleIcon = document.querySelector(".fa-cog");
-const settingBox = document.querySelector(".setting-box");
-const settingBoxToggle = document.querySelector(".setting-box-toggle");
-
-settingBoxToggle.addEventListener("click", togglebox);
-
-function togglebox() {
-    toggleIcon.classList.toggle("fa-spin");
-    settingBox.classList.toggle("open");
-}
-
-// LIGHT MODE ADN DARK MODE
-const colorList = document.querySelectorAll(".mode li");
-let mainColor = localStorage.getItem("color_option");
-let mainFontColor = localStorage.getItem("font_color_option");
-let backgroundColor = localStorage.getItem("background_color_option");
-
-if (mainColor !== null) {
-    document.documentElement.style.setProperty('--main-ele-color', localStorage.getItem(mainColor));
-    document.documentElement.style.setProperty('--main-font-color', localStorage.getItem(mainFontColor));
-    document.documentElement.style.setProperty('--main-bg-color', localStorage.getItem(backgroundColor));
-}
-
-colorList.forEach( li => {
-    li.addEventListener("click", (e) => {
-        document.documentElement.style.setProperty('--main-ele-color', e.target.dataset.color);
-        document.documentElement.style.setProperty('--main-font-color', e.target.dataset.font);
-        document.documentElement.style.setProperty('--main-bg-color', e.target.dataset.background);
-        // LOCAL STORAGE
-        localStorage.setItem(mainColor, e.target.dataset.color);
-        localStorage.setItem(mainFontColor, e.target.dataset.font);
-        localStorage.setItem(backgroundColor, e.target.dataset.background);
-    });
-});
-
 // DROPDOWN
 let dropdownBtn = document.querySelector('.menu-btn');
 let menuContent = document.querySelector('.menu-content');
