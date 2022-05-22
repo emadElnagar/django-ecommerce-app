91f8e1ea12ad05a3782254402f3da34a41600dee
unknown <emad.m.alnagar@gmail.com>
2022-05-22 19:22:48 +0200

    shop needs to be verified by admins to sell

--- accounts/models.py
+++ accounts/models.py
@@ -9,6 +9,7 @@ class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     image = models.ImageField(upload_to='profile/', null=True, blank=True)
     shop = models.CharField(max_length=255, null=True, blank=True)
+    is_shop_verified = models.BooleanField(default=False)
     
     def __str__(self):
         return str(self.user)
